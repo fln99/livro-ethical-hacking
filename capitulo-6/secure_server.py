fd3d1a0cd54c902f1471bdebf473bfff7557cc25
fln99 <59563220+fln99@users.noreply.github.com>
2023-06-06 09:51:49 -0300

    Update secure_server.py and secure_socket.py

--- capitulo-6/secure_server.py
+++ capitulo-6/secure_server.py
@@ -1,20 +1,21 @@
 import socket
 import ssl
 
-client_cert = "../../TLS/client.crt"
-server_key = "../../TLS/server.key"
-server_cert = "../../TLS/server.crt"
-port = 8080
+CLIENT_CERT = "../../TLS/client.crt"
+SERVER_KEY = "../../TLS/server.key"
+SERVER_CERT = "../../TLS/server.crt"
+
+PORT = 8080
 
 context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 context.verify_mode = ssl.CERT_REQUIRED
-context.load_verify_locations(cafile=client_cert)
-context.load_cert_chain(certfile=server_cert, keyfile=server_key)
+context.load_verify_locations(cafile=CLIENT_CERT)
+context.load_cert_chain(certfile=SERVER_CERT, keyfile=SERVER_KEY)
 context.options |= ssl.OP_SINGLE_ECDH_USE
 context.options |= ssl.PROTOCOL_TLS
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
-    sock.bind(("", port))
+    sock.bind(("", PORT))
     sock.listen(1)
 
     with context.wrap_socket(sock, server_side=True) as ssock:
--- capitulo-6/secure_socket.py
+++ capitulo-6/secure_socket.py
@@ -1,22 +1,22 @@
 import socket
 import ssl
 
-client_key = "../../TLS/client.key"
-client_cert = "../../TLS/client.crt"
-server_cert = "../../TLS/server.crt"
-port = 8080
+CLIENT_KEY = "../../TLS/client.key"
+CLIENT_CERT = "../../TLS/client.crt"
+SERVER_CERT = "../../TLS/server.crt"
 
-hostname = "127.0.0.1"
+HOSTNAME = "127.0.0.1"
+PORT = 8080
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLS, cafile=server_cert)
-context.load_cert_chain(certfile=client_cert, keyfile=client_key)
-context.load_verify_locations(cafile=server_cert)
+context = ssl.SSLContext(ssl.PROTOCOL_TLS, cafile=SERVER_CERT)
+context.load_cert_chain(certfile=CLIENT_CERT, keyfile=CLIENT_KEY)
+context.load_verify_locations(cafile=SERVER_CERT)
 context.verify_mode = ssl.CERT_REQUIRED
 context.options |= ssl.OP_SINGLE_ECDH_USE
 context.options |= ssl.PROTOCOL_TLS
 
-with socket.create_connection((hostname, port)) as sock:
-    with context.wrap_socket(sock, server_side=False, server_hostname=hostname) as ssock:
+with socket.create_connection((HOSTNAME, PORT)) as sock:
+    with context.wrap_socket(sock, server_side=False, server_hostname=HOSTNAME) as ssock:
         print(ssock.version())
         message = input("Please enter your message: ")
         ssock.send(message.encode())
